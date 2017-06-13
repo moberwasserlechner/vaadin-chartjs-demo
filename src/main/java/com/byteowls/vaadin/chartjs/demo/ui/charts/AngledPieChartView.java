f3e71d1bb97335482234474cd12c5f9a1f42fb80
Michael Oberwasserlechner <michael@byteowls.com>
2017-06-13 17:55:29 +0200

    refactor(): use other settings for angled pie chart

--- src/main/java/com/byteowls/vaadin/chartjs/demo/ui/charts/AngledPieChartView.java
+++ src/main/java/com/byteowls/vaadin/chartjs/demo/ui/charts/AngledPieChartView.java
@@ -26,9 +26,12 @@ public class AngledPieChartView extends AbstractChartView {
         config
             .data()
                 .labels("Red", "Green", "Yellow", "Grey", "Dark Grey")
-                .addDataset(new PieDataset().label("Dataset 1"))
+                .addDataset(new PieDataset().label("Dataset 1")
+                    .borderWidth(3)
+                    .borderColor("#000")
+                    .randomBackgroundColors(true))
                 .and();
-        
+
         config.
             options()
                 .responsive(true)
@@ -43,27 +46,24 @@ public class AngledPieChartView extends AbstractChartView {
                     .animateRotate(true)
                     .and()
                .done();
-        
+
         List<String> labels = config.data().getLabels();
         for (Dataset<?, ?> ds : config.data().getDatasets()) {
             PieDataset lds = (PieDataset) ds;
             List<Double> data = new ArrayList<>();
-            List<String> colors = new ArrayList<>();
             for (int i = 0; i < labels.size(); i++) {
                 data.add((double) (Math.round(Math.random() * 100)));
-                colors.add(ColorUtils.randomColor(0.7));
             }
-            lds.backgroundColor(colors.toArray(new String[colors.size()]));
             lds.dataAsList(data);
         }
-        
+
         ChartJs chart = new ChartJs(config);
         chart.setJsLoggingEnabled(true);
         chart.addClickListener((a,b) -> {
             PieDataset dataset = (PieDataset) config.data().getDatasets().get(a);
             DemoUtils.notification(a, b, dataset);
         });
-        return chart; 
+        return chart;
     }
 
 }
