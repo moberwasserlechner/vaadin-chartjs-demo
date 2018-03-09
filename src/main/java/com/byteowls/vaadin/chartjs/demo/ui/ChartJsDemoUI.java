e9837a94c5a7714b6b98488759023c957424a731
Michael Oberwasserlechner <michael@byteowls.com>
2018-03-09 11:29:01 +0100

    chore(): other donation text

--- src/main/java/com/byteowls/vaadin/chartjs/demo/ui/ChartJsDemoUI.java
+++ src/main/java/com/byteowls/vaadin/chartjs/demo/ui/ChartJsDemoUI.java
@@ -104,7 +104,7 @@ public class ChartJsDemoUI extends UI {
                 + "| "+env.getProperty("addon.jslib.title")+": <strong>" + env.getProperty("addon.jslib.version") + "</strong> "
                 + "| Vaadin: <strong>" + env.getProperty("addon.vaadin.version") + "</strong> "
                 + "| <a href=\""+env.getProperty("addon.github")+"\">Check it out on Github</a>"
-                + "| <a href=\"https://www.paypal.me/moberwasserlechner/25\">Try donating. It feels good.</a>"
+                + "| <a href=\"https://www.paypal.me/moberwasserlechner/\">Make a donation</a>"
         );
         info.setContentMode(ContentMode.HTML);
 
